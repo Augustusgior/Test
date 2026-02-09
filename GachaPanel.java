11339ceb76514521325443a71346d3b597d90e51
Augustusgior <narawich.kantajai@gmail.com>
2026-02-09 15:56:36 +0700

    test

---
+++ GachaPanel.java
@@ -0,0 +1,25 @@
+// import javax.swing.*;
+
+// public class GachaPanel extends JPanel {
+
+//     private String result;
+
+//     public GachaPanel(GameFrame frame, StorePanel store) {
+//         setBounds(20, 70, 400, 450);
+//         setBorder(BorderFactory.createTitledBorder("Gacha"));
+
+//         JButton roll = new JButton("ROLL");
+//         JButton use = new JButton("USE");
+
+//         roll.addActionListener(e -> {
+//             result = "Knight"; // สมมติ
+//         });
+
+//         use.addActionListener(e -> {
+//             frame.data.selectedCharacter = result;
+//         });
+
+//         add(roll);
+//         add(use);
+//     }
+// }
